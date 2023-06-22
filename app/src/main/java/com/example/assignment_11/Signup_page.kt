a86a0a8d2c533b66296248ad59476ec450af4ac8
Goh Wai Jian <gwaijian@gmail.com>
2023-06-22 12:03:38 +0800

    add if else to Signup_page

--- app/src/main/java/com/example/assignment_11/Signup_page.kt
+++ app/src/main/java/com/example/assignment_11/Signup_page.kt
@@ -5,21 +5,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import kotlin.math.sign
 
 class Signup_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_page)
 
-        val signupBtn =findViewById<Button>(R.id.signup_button)
+        val signupBtn = findViewById<Button>(R.id.signup_button)
+        val name = findViewById<TextView>(R.id.username_signUp_editText)
+        val email =findViewById<TextView>(R.id.email_signUp_editText)
+        val password = findViewById<TextView>(R.id.password_signUp_editText)
 
-        signupBtn.setOnClickListener {
-            val intent = Intent(this, Signup_page2::class.java)
-            val name =findViewById<TextView>(R.id.username_signUp_editText)
-            val Name = name.text.toString()
-            val message = "$Name, We are really sorry. The feature is not implemented yet."
-            intent.putExtra("MGS_TO_USER", message)
-            startActivity(intent)
+        //if name email and password is empty toast "Please fill all the fields" else go to signup_page2
+
+        signupBtn.setOnClickListener{
+            val name = name.text.toString()
+            val email = email.text.toString()
+            val password = password.text.toString()
+
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Please fill all the fields",Toast.LENGTH_LONG).show()
+            }else{
+                val intent = Intent(this, Signup_page2::class.java)
+                val message = "$name, We are really sorry. The feature is not implemented yet."
+                intent.putExtra("MGS_TO_USER", message)
+                startActivity(intent)
+            }
+
         }
+
+
     }
 }
